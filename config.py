a520a2caf903c6966046e4cefd7e2a48b8768e6d
SluffyG1 <sluffyg1@gmail.com>
2024-11-18 21:59:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ class Config(object):
     START_PIC   = os.environ.get("START_PIC", "https://i.ibb.co/ZLbRGmT/Picsart-24-02-16-14-30-48-873.png")
     ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '6813998583').split()]
     # -- FORCE_SUB_CHANNELS = ["BotzPW","AshuSupport","AshutoshGoswami24"] -- # 
-    FORCE_SUB_CHANNELS = os.environ.get('FORCE_SUB_CHANNELS', 'Old_Bollywood_movie_HD').split(',')
+    FORCE_SUB_CHANNELS = os.environ.get('FORCE_SUB_CHANNELS', 'The_Anime_Saga').split(',')
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002437449273"))
     PORT = int(os.environ.get("PORT", "8040"))
     
